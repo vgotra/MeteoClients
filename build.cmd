449f33b45b4711b1462b8fbcdd3615f68803e17a
Volodymyr Gotra <vgotra@gmail.com>
2016-09-09 01:24:41 +0300

    Changes for build

--- build.cmd
+++ build.cmd
@@ -4,6 +4,6 @@ dotnet restore
 
 :: dotnet build **/*/project.json
 
-dotnet build **/*/project.json -r win10-x64 
-dotnet build **/*/project.json -r osx.10.11-x64 
-dotnet build **/*/project.json -r ubuntu.16.04-x64 
+dotnet build **/*/*/project.json -r win10-x64 
+dotnet build **/*/*/project.json -r osx.10.11-x64 
+dotnet build **/*/*/project.json -r ubuntu.16.04-x64 
