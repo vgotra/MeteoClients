67e196d6d801418a95e39b175de0c06a99bd9432
Volodymyr Gotra <vgotra@gmail.com>
2016-09-09 01:23:50 +0300

    Changes for build

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@ dotnet restore
 # dotnet test ./test/TEST_PROJECT_NAME -c Release -f netcoreapp1.0
 
 # Instead, run directly with mono for the full .net version 
-dotnet build **/*/project.json
+# dotnet build **/*/*/project.json
 
-# dotnet build **/*/project.json -r win10-x64 
-# dotnet build **/*/project.json -r osx.10.11-x64 
-# dotnet build **/*/project.json -r ubuntu.16.04-x64 
+dotnet build **/*/*/project.json -r win10-x64 
+dotnet build **/*/*/project.json -r osx.10.11-x64 
+dotnet build **/*/*/project.json -r ubuntu.16.04-x64 
 
 #revision=${TRAVIS_JOB_ID:=1}  
 #revision=$(printf "%04d" $revision) 
